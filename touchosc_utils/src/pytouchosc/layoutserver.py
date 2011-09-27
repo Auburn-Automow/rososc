dc63e0be28c0e50c7f6e3d3f1778979cd563d471
Michael Carroll <carroll.michael@gmail.com>
2011-09-27 15:00:27 -0500

    Changing my pytouchosc class to include the layoutserver class

--- touchosc_utils/src/pytouchosc/layoutserver.py
+++ touchosc_utils/src/pytouchosc/layoutserver.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
 # layout_sync
 #
@@ -30,22 +28,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"""
 
-Python script and helper class for loading layouts to TouchOSC.
-
-layout_sync.py provides a command line tool for programmatically loading TouchOSC
-layout files to the TouchOSC iPhone/iPad application without the need for the 
-TouchOSC editor program.  
-
-This was reverse-engineered from Wireshark logs of the actual TouchOSC layout 
-editor.  The editor regsiters a bonjour service of type _touchosc._tcp and then 
-
-"""
-
-__author__ = "Michael Carroll <carroll.michael@gmail.com>"
-
-import roslib; roslib.load_manifest('touchosc_utils')
 import bonjour
 
 import sys
@@ -54,42 +37,63 @@ import zipfile
 import os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-def make_layoutHandler_class(layoutFile):
+def make_layoutHandler_class(layoutName,layoutFile):
     """
     Function to generate a class that extends BaseHTTPRequest Handler.
 
-    @type layoutFile: str
-    @param layoutFile: Path to the layout file to be loaded to the TouchOSC app.
+    @type layoutName: str
+    @param layoutName: Name of the layout file (e.g. sample.touchosc)
+    @type layoutFile: File
+    @param layoutFile: The layout file to be loaded to the TouchOSC app.
     """
     #TODO: Do HTTPRequestHandler the proper "Python way".
     # This is sort of a hack.  The way that this should probably be done:
     # 1. Extend the HTTPServer class to take "layoutFile" as part of the constructor.
     # 2. In my overridden do_GET function in the LayoutHTTPRequestHandler, I should
     # then take advantage of the instance variable "server" which points to the 
-    # overrideen HTTPServer class.  I should be able to get to all of the instance
+    # over ridden HTTPServer class.  I should be able to get to all of the instance
     # variables of the HTTPServer class through that "server" variable.
     #
     # That is what I tried, but couldn't get working.  Because this is not a really
     # mission-critical application, this will work for now.
     class LayoutHTTPRequestHandler(BaseHTTPRequestHandler):
         def do_GET(self):
-            f = zipfile.ZipFile(layoutFile,"r")
-            print(f.read("index.xml"))
             try:
                 self.send_response(200)
                 self.send_header('Content-type','application/touchosc')
                 self.send_header('Content-Disposition',
-                                 'attachment; filename="%s"'%(
-                                     os.path.basename(layoutFile)))
+                                 'attachment; filename="%s"'%layoutName)
                 self.end_headers()
-                self.wfile.write(f.read("index.xml"))
+                self.wfile.write(layoutFile)
             except Exception:
                 import traceback
                 traceback.print_exc(f=sys.stdout)   
             return
     return LayoutHTTPRequestHandler
 
-def main(argv, stdout):
+class LayoutServer(object):
+    def __init__(self,layoutPath,name,port):
+        if not os.path.lexists(layoutPath):
+            raise ValueError("Layout file not found: %s",layoutPath)
+        layoutZip = zipfile.ZipFile(layoutPath,"r")
+        layoutFile = layoutZip.read("index.xml")
+        layoutName = os.path.basename(layoutPath)
+        self.bonjourServer = bonjour.Bonjour(name,port,
+                                             '_touchosceditor._tcp')
+        self.httpServer = HTTPServer(('',port),
+                                     make_layoutHandler_class(layoutName,
+                                                              layoutFile))
+
+    def run(self):
+        self.bonjourServer.run_register()
+        self.httpServer.serve_forever()
+    
+    def stop(self):
+        self.httpServer.socket.close()
+        self.bonjourServer.stop_register()
+        pass
+
+def main(argv,stdout):
     usage = "usage: %prog [options] /path/to/layout.touchosc"
     parser = OptionParser(usage=usage)
     parser.add_option("-p","--port",action="store",type="int",dest="port",
@@ -98,35 +102,24 @@ def main(argv, stdout):
     parser.add_option("-n","--name",action="store",type="string",dest="name",
             default="OSC Layout Server on %s"%socket.gethostname(),
             help="Name that will appear in TouchOSC's Server List")
-
     (options,args) = parser.parse_args(argv)
     if len(args) < 2:
         parser.error("Please specify a layout file.")
         sys.exit(1)
-    
-    layoutFilePath=args[1];
-
-    if not os.path.lexists(layoutFilePath):
-        parser.error("Layout file not found: %s"%(layoutFilePath))
-        sys.exit(1)
-
-
-    b = bonjour.bonjour(options.name,options.port,'_touchosceditor._tcp')
-    b.run_register()
-    server = HTTPServer(('',options.port),make_layoutHandler_class(args[1]))
-
+    layoutFilePath = args[1];    
     try:
-        server.serve_forever()
+        server = LayoutServer(layoutFilePath,options.name,options.port)
+    except ValueError:
+        parser.error("Layout file not found: %s",layoutFilePath)
+        sys.exit(1)
+    
+    try:
+        server.run()
     except KeyboardInterrupt:
         pass
     finally:
-        server.socket.close()
-        b.stop_register()
-
-
+        server.stop()
 
 if __name__ == '__main__':
     from optparse import OptionParser
-
     main(sys.argv, sys.stdout)
-
