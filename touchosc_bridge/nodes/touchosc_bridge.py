9901ef2783f0337269b9846d31c0d7dd43a2814c
John Harrison <ash@greaterthaninfinity.com>
2011-11-03 13:14:01 -0500

    Cleaning up some code maybe? /shrug.

--- touchosc_bridge/nodes/touchosc_bridge.py
+++ touchosc_bridge/nodes/touchosc_bridge.py
@@ -28,6 +28,28 @@ def walkNode(node, path='', sep='/'):
             newpath = path
         walkNode(v,newpath)
 
+def walk_node(parent, sep='/'):
+    """Walk a node tree for nodes with callbacks."""
+    consumer = [parent._osc_receiver]
+    foo = {}
+    sep = '/'
+    while consumer:
+        node = consumer.pop(0)
+        if len(node._childNodes) == 0 and len(node._callbacks):
+            cb = node._callbacks.pop()
+            cbStr = ".".join([cb.__module__,cb.__name__])
+            yield (build_path(node, sep), cbStr)
+        else:
+            for k, v in node._childNodes.iteritems():
+                consumer.append(v)
+
+def build_path(node, sep):
+    """Reconstruct a path by following the parents of each node."""
+    if node._parent:
+        return build_path(node._parent, sep) + sep + node.getName()
+    else:
+        return ''
+
 if __name__=="__main__":
     def start():
         try:         
@@ -35,8 +57,8 @@ if __name__=="__main__":
             t = TouchOSCNode(name, port=8000)
             t.addTabpageHandler(TeleopTabpageHandler(name,"teleop",
                                                      ["ipod/teleop","ipad/teleop"]))
-            walkNode(t._osc_receiver)
-            print t._osc_receiver._childNodes
+            foo = {k: v for k, v in walk_node(t)}
+            print foo, 'done'
             reactor.callLater(0.5, t.initializeTabpages)
         except:
             import traceback
