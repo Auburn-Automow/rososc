31ee40090785f122fe80ccd8273764d3f5b28cdf
Michael Carroll <carroll.michael@gmail.com>
2011-12-11 17:42:47 -0600

    Adding handler loading back in.

--- teleop_handler/src/teleop_handler/teleoptabpage.py
+++ teleop_handler/src/teleop_handler/teleoptabpage.py
@@ -4,69 +4,71 @@ import rospy
 from txosc import osc
 from geometry_msgs.msg import Twist
 
-from touchoscnode import AbstractTabpageHandler
+from touchosc_bridge.abstracttabpage import AbstractTabpageHandler
 from twisted.internet import reactor
 
 import socket
 
 class TeleopTabpageHandler(AbstractTabpageHandler):
-    def __init__(self, nodeName, tabpageName, tabpageAlias, 
-                 max_vx = 0.6,max_vy = 0.6,max_vw = 0.8,
-                 max_run_vx = 1.0, max_run_vy = 1.0, max_run_vw = 1.0,
-                 run = False):
-        super(TeleopTabpageHandler, self).__init__(nodeName, tabpageName, tabpageAlias)
-                
-        pref = "~" + tabpageName + "/"
-        self.max_vx = rospy.get_param(pref+"max_vx",max_vx)
-        self.max_vy = rospy.get_param(pref+"max_vy",max_vy)
-        self.max_vw = rospy.get_param(pref+"max_vw",max_vw)
-        self.max_run_vx = rospy.get_param(pref+"max_run_vx",max_run_vx)
-        self.max_run_vy = rospy.get_param(pref+"max_run_vy",max_run_vy)
-        self.max_run_vw = rospy.get_param(pref+"max_run_vw",max_run_vw)
-        self.running = rospy.get_param(pref+"run", run)
-        self.minPublishFreq = rospy.get_param(pref+"min_freq",10) 
-        
-        self.pub = rospy.Publisher("cmd_vel",Twist)
-        
-        self.addOscCallback('xy', self.xypad_callback)
-        self.addOscCallback('w', self.w_callback)
-        self.addOscCallback('control', self.control_callback)
-        self.addOscCallback('turbo', self.turbo_callback)
-        
+    def __init__(self, touchosc_interface, handler_name, tabpage_names,
+                 max_vx=0.6, max_vy=0.6, max_vw=0.8,
+                 max_run_vx=1.0, max_run_vy=1.0, max_run_vw=1.0,
+                 run=False):
+        super(TeleopTabpageHandler, self).__init__(touchosc_interface,
+                                                   handler_name,
+                                                   tabpage_names)
+
+        pref = "~" + self.handler_name + "/"
+        self.max_vx = rospy.get_param(pref + "max_vx", max_vx)
+        self.max_vy = rospy.get_param(pref + "max_vy", max_vy)
+        self.max_vw = rospy.get_param(pref + "max_vw", max_vw)
+        self.max_run_vx = rospy.get_param(pref + "max_run_vx", max_run_vx)
+        self.max_run_vy = rospy.get_param(pref + "max_run_vy", max_run_vy)
+        self.max_run_vw = rospy.get_param(pref + "max_run_vw", max_run_vw)
+        self.running = rospy.get_param(pref + "run", run)
+        self.minPublishFreq = rospy.get_param(pref + "min_freq", 10)
+
+        self.pub = rospy.Publisher("cmd_vel", Twist)
+
+        self.add_osc_callback('xy', self.xypad_callback)
+        self.add_osc_callback('w', self.w_callback)
+        self.add_osc_callback('control', self.control_callback)
+        self.add_osc_callback('turbo', self.turbo_callback)
+
         self.cmd = Twist()
         self.cmd.linear.x = 0.0
         self.cmd.linear.y = 0.0
         self.cmd.angular.z = 0.0
-        
+
         self.masterOsc = None
-        reactor.callLater(1.0/self.minPublishFreq, self.publish_cmd)
-        
+        reactor.callLater(1.0 / self.minPublishFreq, self.publish_cmd)
+
     def initializeTabpage(self):
         self.zero_command()
-        
+
     def zero_command(self):
-        self.sendToAll(osc.Bundle([osc.Message('xy',0.0,0.0),
-                                   osc.Message('w',0.0),
-                                   osc.Message('control',0.0),
+        self.sendToAll(osc.Bundle([osc.Message('xy', 0.0, 0.0),
+                                   osc.Message('w', 0.0),
+                                   osc.Message('control', 0.0),
                                    osc.Message('turbo', 0.0),
-                                   osc.Message('master',"")]))
-        
+                                   osc.Message('master', "")]))
+
         self.cmd.linear.x = 0.0
         self.cmd.linear.y = 0.0
         self.cmd.angular.z = 0.0
-        self.running = False    
-        
+        self.running = False
+
     def publish_cmd(self):
         if self.masterOsc and self.masterOsc not in self.activeClients:
-                self.__oscSendToAll(osc.Bundle([osc.Message('control',0.0),
-                                           osc.Message('master','')]))
+                self.__oscSendToAll(osc.Bundle([osc.Message('control', 0.0),
+                                           osc.Message('master', '')]))
                 self.masterOsc = None
         elif self.masterOsc:
-            self.sendToClient(osc.Message('control',1.0), self.masterOsc)
+            self.sendToClient(osc.Message('control', 1.0), self.masterOsc)
 
         self.pub.publish(self.cmd)
-        reactor.callLater(1.0/self.minPublishFreq, self.publish_cmd)
-    
+        reactor.callLater(1.0 / self.minPublishFreq, self.publish_cmd)
+
     def xypad_callback(self, addressList, valueList, sendAddress):
         if sendAddress[0] not in self.activeClients:
                 self.activeClients[sendAddress[0]] = addressList[0]
@@ -75,8 +77,8 @@ class TeleopTabpageHandler(AbstractTabpageHandler):
                                       self.masterOsc)
             vx = self.max_run_vx if self.running else self.max_vx
             vy = self.max_run_vy if self.running else self.max_vy
-            self.cmd.linear.x = max(min(valueList[0] * vx,vx),-vx)
-            self.cmd.linear.y = max(min(valueList[1] * -vy,vy),-vy)
+            self.cmd.linear.x = max(min(valueList[0] * vx, vx), -vx)
+            self.cmd.linear.y = max(min(valueList[1] * -vy, vy), -vy)
         elif sendAddress[0] == self.masterOsc and valueList[0] == 0.0:
             self.cmd.linear.x = 0.0
             self.cmd.linear.y = 0.0
@@ -89,50 +91,50 @@ class TeleopTabpageHandler(AbstractTabpageHandler):
             self.sendToAllOtherActive(osc.Message('w', *valueList),
                                       self.masterOsc)
             vw = self.max_run_vw if self.running else self.max_vw
-            self.cmd.angular.z = max(min(valueList[0] * -vw,vw),-vw)
+            self.cmd.angular.z = max(min(valueList[0] * -vw, vw), -vw)
         elif sendAddress[0] == self.masterOsc and valueList[0] == 0.0:
             self.cmd.angular.z = 0.0
         self.pub.publish(self.cmd)
-        
+
     def control_callback(self, addressList, valueList, sendAddress):
         if len(addressList) == 2:
             if sendAddress[0] not in self.activeClients:
                 self.activeClients[sendAddress[0]] = addressList[0]
                 if not self.masterOsc:
                     name = self.masterOsc
-                    self.sendToClient(osc.Message('control',1.0), self.masterOsc)
-                    self.sendToAll(osc.Message('master',name))
+                    self.sendToClient(osc.Message('control', 1.0), self.masterOsc)
+                    self.sendToAll(osc.Message('master', name))
             elif not self.masterOsc and valueList[0] == 1.0:
                 self.masterOsc = sendAddress[0]
                 name = self.masterOsc
-                self.sendToClient(osc.Message('control',1.0), self.masterOsc)
-                self.sendToAll(osc.Message('master',name))
+                self.sendToClient(osc.Message('control', 1.0), self.masterOsc)
+                self.sendToAll(osc.Message('master', name))
             elif valueList[0] == 0.0:
-                self.sendToAll(osc.Message('control',0.0))
-                self.sendToAll(osc.Message('master',''))
+                self.sendToAll(osc.Message('control', 0.0))
+                self.sendToAll(osc.Message('master', ''))
                 self.masterOsc = None
-                    
+
     def turbo_callback(self, addressList, valueList, sendAddress):
         if sendAddress[0] not in self.activeClients:
                 self.activeClients[sendAddress[0]] = addressList[0]
         if sendAddress[0] == self.masterOsc and len(addressList) == 2:
             if valueList[0] == 1.0:
-                message = osc.Message("turbo",valueList[0])
+                message = osc.Message("turbo", valueList[0])
                 self.sendToAll(message)
                 self.running = True
             elif valueList[0] == 0.0:
-                message = osc.Message("turbo",valueList[0])
+                message = osc.Message("turbo", valueList[0])
                 self.sendToAll(message)
                 self.running = False
-    
+
     def tabpageClosedCallback(self, client, tabpage):
         if client[0] in self.activeClients:
             del self.activeClients[client[0]]
         if client[0] == self.masterOsc:
-            self.sendToAll(osc.Bundle([osc.Message('control',0.0),
-                                       osc.Message('master','')]))
+            self.sendToAll(osc.Bundle([osc.Message('control', 0.0),
+                                       osc.Message('master', '')]))
             self.masterOsc = None
-            
 
-        
-        
+
+
+
--- touchosc_bridge/nodes/touchosc_bridge.py
+++ touchosc_bridge/nodes/touchosc_bridge.py
@@ -14,13 +14,44 @@ from touchosc_bridge.defaulttabpage import DefaultTabpageHandler
 
 import pytouchosc
 
+def load_handler(package, handler_class):
+    try:
+        roslib.load_manifest(package)
+    except:
+        rospy.logerr("Failed to load manifest for package %s" % package)
+    try:
+        (module, cls) = handler_class.split("/")
+        m = __import__(package + "." + module, fromlist=[cls])
+        handler = m.__getattribute__(cls)
+    except:
+        rospy.logerr("Cannot import package: %s" % package + "." + module)
+        rospy.logerr("sys.path was " + str(sys.path))
+        return None
+    return handler
+
 if __name__ == "__main__":
     def start():
         try:
             t = TouchOscInterface()
             default = rospy.get_param("~load_default", False)
             handlers = rospy.get_param("~handlers", None)
-            
+
+            loaded_handlers = {}
+
+            if handlers:
+                rospy.loginfo("Adding handlers from configuration")
+                for handler in handlers:
+
+                    params = rospy.get_param("~" + handler)
+                    HandlerClass = load_handler(params['pkg'],
+                                                params['class'])
+                    aliases = [handler] + params['tabpage_aliases']
+                    rospy.loginfo("Adding %s handler with names %s" % (handler, aliases))
+                    loaded_handlers[handler] = HandlerClass(t,
+                                                           handler,
+                                                           aliases)
+                    t.register_handler(loaded_handlers[handler])
+
             if default:
                 names = set()
                 default_handlers = {}
@@ -30,15 +61,17 @@ if __name__ == "__main__":
                 if not layout_file and not handlers:
                     rospy.logfatal("""No named handlers and no layout file
                         specified, rososc will not operate""")
+                    return
                 elif not layout_file:
                     rospy.logerr("""No layout file specified, default tabpage
                         handler will not operate.""")
-                    
+                    return
+
                 layout = pytouchosc.Layout.createFromExisting(layout_file)
                 for tp in layout.getTabpageNames():
                     if tp not in names:
                         names.add(tp)
-                        default_handlers[tp] = DefaultTabpageHandler(t,layout.getTabpage(tp))
+                        default_handlers[tp] = DefaultTabpageHandler(t, layout.getTabpage(tp))
                         t.register_handler(default_handlers[tp])
         except:
             import traceback
